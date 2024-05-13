18f5fa1580e9927843d0efe6fe49ecefd4411f35
Nishant Raj <nishantraj2109@gmail.com>
2024-05-13 21:56:39 +0530

    switch case

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-const student = {
-    name: 'John',
-    age: 24,
-    gender: 'male'
-}
+// const student = {
+//     name: 'John',
+//     age: 24,
+//     gender: 'male'
+// }
 
-console.log(student.name +"\n" + student.age);
+// console.log(student.name +"\n" + student.age);
+
+
+// EXPONENTIATION
+// const a = 2;
+// const b = 3;
+
+// console.log(a ** b);
+
+
+// SWITCH CASE
+const day = 'monday';
+switch (day){
+    case 'sunday': 
+        console.log("sunday");
+        break;
+    case 'tuesday':
+        console.log("tuesday");
+        break;
+    default: 
+        console.log("default")
+}
