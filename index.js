52074f966591e022b197772a2069a81b29f82c94
Nishant Raj <nishantraj2109@gmail.com>
2024-05-14 15:06:39 +0530

    for in loop

--- index.js
+++ index.js
@@ -6,27 +6,24 @@
 
 // console.log(student.name +"\n" + student.age);
 
-
 // EXPONENTIATION
 // const a = 2;
 // const b = 3;
 
 // console.log(a ** b);
 
-
 // SWITCH CASE
 // const day = 'monday';
 // switch (day){
-//     case 'sunday': 
+//     case 'sunday':
 //         console.log("sunday");
 //         break;
 //     case 'tuesday':
 //         console.log("tuesday");
 //         break;
-//     default: 
+//     default:
 //         console.log("default")
 // }
-
 
 // get user to input a number using prompt. check if number is multiple of 5 or not
 // let number = prompt("Enter a number:");
@@ -34,13 +31,24 @@
 //     console.log("even")
 // }else if(number % 2 !== 0){
 //     console.log("odd");
-// }   
-
+// }
 
 //make an array
-let arr = [1,2,3,4,5,6,7,8,9,10, 29];
+// let arr = [1,2,3,4,5,6,7,8,9,10, 29];
 
-for(let i of arr){
-    console.log(i);
+// for(let i of arr){
+//     console.log(i);
+// }
+// console.log("size of array: " + arr.length);
+
+
+// FOR IN loop
+let student = {
+  name: "Noddy",
+  age: 23,
+  location: "jaipur",
+};
+
+for(let key in student) {
+  console.log(key + " " + student[key]);
 }
-console.log("size of array: " + arr.length);
